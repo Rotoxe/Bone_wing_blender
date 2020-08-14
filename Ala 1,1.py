6c5f9d3ac06d48d893ff72fbc1b357da42238558
Rotoxe <59949309+Rotoxe@users.noreply.github.com>
2020-08-13 20:41:13 -0500

    Update Ala 1,1.py
    Agregando IK a los huesos

--- Ala 1,1.py
+++ Ala 1,1.py
@@ -17,8 +17,19 @@ import bpy
 bpy.ops.object.armature_add(enter_editmode=True, align='WORLD', location=(2, 0, 1)) 
 bpy.ops.transform.translate(value=(1, 0, 0), orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(True, False, False), mirror=True, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False)
 bpy.ops.armature.extrude_move(ARMATURE_OT_extrude={"forked":False}, TRANSFORM_OT_translate={"value":(2, 0, -2), "orient_type":'GLOBAL', "orient_matrix":((1, 0, 0), (0, 1, 0), (0, 0, 1)), "orient_matrix_type":'GLOBAL', "constraint_axis":(False, False, False), "mirror":True, "use_proportional_edit":False, "proportional_edit_falloff":'SMOOTH', "proportional_size":1, "use_proportional_connected":False, "use_proportional_projected":False, "snap":False, "snap_target":'CLOSEST', "snap_point":(0, 0, 0), "snap_align":False, "snap_normal":(0, 0, 0), "gpencil_strokes":False, "cursor_transform":False, "texture_space":False, "remove_on_cancel":False, "release_confirm":False, "use_accurate":False})
+bpy.ops.object.posemode_toggle()
+bpy.ops.pose.constraint_add(type='IK')
+bpy.context.object.pose.bones["Bone.001"].constraints["IK"].chain_count = 2
+bpy.ops.object.editmode_toggle()
 bpy.ops.armature.extrude_move(ARMATURE_OT_extrude={"forked":False}, TRANSFORM_OT_translate={"value":(2, 0, 2), "orient_type":'GLOBAL', "orient_matrix":((1, 0, 0), (0, 1, 0), (0, 0, 1)), "orient_matrix_type":'GLOBAL', "constraint_axis":(False, False, False), "mirror":True, "use_proportional_edit":False, "proportional_edit_falloff":'SMOOTH', "proportional_size":1, "use_proportional_connected":False, "use_proportional_projected":False, "snap":False, "snap_target":'CLOSEST', "snap_point":(0, 0, 0), "snap_align":False, "snap_normal":(0, 0, 0), "gpencil_strokes":False, "cursor_transform":False, "texture_space":False, "remove_on_cancel":False, "release_confirm":False, "use_accurate":False})
+bpy.ops.object.posemode_toggle()
+bpy.ops.pose.constraint_add(type='IK')
+bpy.context.object.pose.bones["Bone.002"].constraints["IK"].chain_count = 2
+bpy.ops.object.editmode_toggle()
 bpy.ops.armature.extrude_move(ARMATURE_OT_extrude={"forked":False}, TRANSFORM_OT_translate={"value":(4, 0, 0), "orient_type":'GLOBAL', "orient_matrix":((1, 0, 0), (0, 1, 0), (0, 0, 1)), "orient_matrix_type":'GLOBAL', "constraint_axis":(False, False, False), "mirror":True, "use_proportional_edit":False, "proportional_edit_falloff":'SMOOTH', "proportional_size":1, "use_proportional_connected":False, "use_proportional_projected":False, "snap":False, "snap_target":'CLOSEST', "snap_point":(0, 0, 0), "snap_align":False, "snap_normal":(0, 0, 0), "gpencil_strokes":False, "cursor_transform":False, "texture_space":False, "remove_on_cancel":False, "release_confirm":False, "use_accurate":False})
+bpy.ops.object.posemode_toggle()
+bpy.ops.pose.constraint_add(type='IK')
+bpy.context.object.pose.bones["Bone.003"].constraints["IK"].chain_count = 2
 
 #cambiar el nombre del Armature, 
 bpy.ops.object.editmode_toggle()#primero cambia de vista a edicion de objeto
@@ -36,8 +47,14 @@ for name, newname in namelist:
         Continue
     #renombrar
     pb.name = newname
-#poner IK
+
 bpy.ops.object.posemode_toggle()#pasa a pantalla de pose
+
+
+#poner IK
+#bpy.ops.pose.constraint_add(type='IK')
+#bpy.context.object.pose.bones["HAla"].constraints["IK"].chain_count = 2
+
 #falta linea de "seleccionar objeto
 #bpy.context.space_data.context = 'BONE_CONSTRAINT'
 #bpy.ops.pose.constraint_add(type='IK')
